ddbe5423b0c3194c14107350df9816eee41cdb7f
DUPROS Leegan <leegan.dupros@epitech.eu>
2024-11-27 00:09:43 +0100

    Initial Commit

---
+++ Meme Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meme Generator
+//
+//  Created by dleegan on 27/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meme Generator/Meme_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_GeneratorApp.swift
+//  Meme Generator
+//
+//  Created by dleegan on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meme_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
